9d9a65ea35542ed22a59014becf6e1981595af73
Marybeee <67306791+Marybeee@users.noreply.github.com>
2024-01-19 11:57:25 +0000

    onboarding page done

--- frontend/src/pages/onboarding.tsx
+++ frontend/src/pages/onboarding.tsx
@@ -1,17 +1,20 @@
 import Layout from "@/components/Layout";
-import Image from "next/image";
+import OnboardingElement from "@/components/Onboarding/OnboardingElement";
 import React from "react";
 
 
 export default function OnboardingPage()
 {
     return <Layout>
-          <div className="grid grid-cols-2 w-full h-full">
-            <Image src='/student.png' width={500} height={500} alt="student" />
-            <Image src='/teacher.png' width={500} height={500} alt="teacher" />
-            <Image src='/teacher.png' width={500} height={500} alt="parent" />
-            <Image src='/school.png' width={500} height={500} alt="school" />
+        <div className="flex flex-col justify-center items-center mx-[200px] my-12">
+        <div className="grid grid-cols-2 w-full h-full gap-4 ">
+        <OnboardingElement name="Student" buttonContent="Join your class" iconSvg="/rocket.svg" bgImage="/student.png" route="/student" />
+        <OnboardingElement name="Teacher" buttonContent="Set the crew" iconSvg="/space.svg" bgImage="/teacher.png" route="/teacher" />
+        <OnboardingElement name="Parent" buttonContent="Get Started" iconSvg="/meme.svg" bgImage="/teacher.png" route="/parent" />
+        <OnboardingElement name="School" buttonContent="Get Started" iconSvg="/earth.svg" bgImage="/student.png" route="/student" />
         </div>
+        </div>
+        
     </Layout>
   
 }
