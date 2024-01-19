7cb6330cc7d049f4890e3ae15938140e387c86b9
Marybeee <67306791+Marybeee@users.noreply.github.com>
2024-01-19 09:35:33 +0000

    add logo,setup home and connectkit

--- frontend/src/components/Dashboard/index.tsx
+++ frontend/src/components/Dashboard/index.tsx
@@ -1,5 +1,14 @@
+import Image from "next/image";
 import React from "react";
 
 export default function Home() {
-  return <div></div>;
+  return <div className="flex flex-col justify-center items-center">
+    <div className="bg-gradient-to-r from-[#F8049C] to-[#FDD54F] w-full h-[500px] rounded-b-full flex flex-col justify-center items-center">
+    <Image src={'/logo.png'} width={200} height={100} alt="logo" />
+    <p className="text-white text-lg">DeFi for Teens</p>
+    <p className="text-white  text-3xl font-semibold mt-14">Hi Welcome</p>
+  </div>
+  <button>Connect Family Wallet</button>
+
+  </div> 
 }
--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -3,6 +3,25 @@
 import * as React from "react";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+
+import { WagmiConfig, createConfig } from "wagmi";
+import { ConnectKitProvider, ConnectKitButton, getDefaultConfig } from "connectkit";
+
+
+const config = createConfig(
+  getDefaultConfig({
+    alchemyId: process.env.NEXT_PUBLIC_ALCHEMY_ID || "", 
+    walletConnectProjectId: process.env.NEXT_PUBLIC_PROJECT_ID || "",
+    appName: "Savvy",
+    appDescription: "DeFi for Teens",
+    appUrl: "https://savvy.vercel.app", 
+    appIcon: "/logo.png",
+  }),
+);
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return <WagmiConfig config={config}>
+          <ConnectKitProvider>
+      <Component {...pageProps} /> </ConnectKitProvider>
+    </WagmiConfig>;
 }
