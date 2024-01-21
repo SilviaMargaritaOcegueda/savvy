d5a8328ac611fb0ce3ec815805fdfac54ba084cf
Marybeee <maria.behappy.mb@gmail.com>
2024-01-21 07:57:16 +0100

    design adjustment

--- frontend/src/components/Onboarding/index.tsx
+++ frontend/src/components/Onboarding/index.tsx
@@ -1,15 +1,39 @@
 import React from "react";
 import OnboardingElement from "./OnboardingElement";
 
-
-export default function Onboarding()
-{
-    return <div className="flex flex-col justify-center items-center mx-[200px] my-12">
-    <div className="grid grid-cols-2 w-full h-full gap-4 ">
-    <OnboardingElement name="Student" buttonContent="Join your class" iconSvg="/rocket.svg" bgImage="/student.png" route="/student" />
-    <OnboardingElement name="Teacher" buttonContent="Set the crew" iconSvg="/space.svg" bgImage="/teacher.png" route="/class/create" />
-    <OnboardingElement name="Parent" buttonContent="Get Started" iconSvg="/meme.svg" bgImage="/teacher.png" route="/parent" />
-    <OnboardingElement name="School" buttonContent="Get Started" iconSvg="/earth.svg" bgImage="/school.png" route="/school/register" />
+export default function Onboarding() {
+  return (
+    <div className="flex flex-col justify-center items-center mx-[200px] my-12">
+      <div className="grid grid-cols-2 w-full h-full gap-4 ">
+        <OnboardingElement
+          name="Student"
+          buttonContent="Join class"
+          iconSvg="/rocket.svg"
+          bgImage="/student.png"
+          route="/student"
+        />
+        <OnboardingElement
+          name="Teacher"
+          buttonContent="Set students"
+          iconSvg="/space.svg"
+          bgImage="/teacher.png"
+          route="/class/create"
+        />
+        <OnboardingElement
+          name="Parent"
+          buttonContent="Create wallet"
+          iconSvg="/meme.svg"
+          bgImage="/teacher.png"
+          route="/parent"
+        />
+        <OnboardingElement
+          name="School"
+          buttonContent="Set classes"
+          iconSvg="/earth.svg"
+          bgImage="/school.png"
+          route="/school/register"
+        />
+      </div>
     </div>
-    </div>
+  );
 }
